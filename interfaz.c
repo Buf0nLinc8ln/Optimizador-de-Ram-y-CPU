8c7cdfd51a224cbb29855c1896d11802a6b0199a
P0ll0P <davidroblescas@gmail.com>
2025-06-28 15:01:47 -0600

    interfaz actualizada y botones agregados

--- interfaz.c
+++ interfaz.c
@@ -1,51 +1,106 @@
 #include <gtk/gtk.h>
 
-GtkWidget *entry_pid, *entry_uid, *entry_gid, *entry_nombre, *entry_ppid, *entry_gpid, *entry_ram;
-GtkWidget *dropdown_estado;
-GtkWidget *textview;
+GtkWidget *entry_pid, *entry_uid, *entry_gid, *entry_nombre, *entry_ppid, *entry_gpid, *entry_estado, *entry_ram;
 GtkWidget *estado_buttons[5];
-const char *estados[] = {"Running", "Sleeping", "Zombie", "T (Suspendido)", "D (Espera)"};
 gint estado_seleccionado = 0;
+GtkWidget *entry_ram_limit;
+GtkWidget *entry_cpu_limit;
+GtkWidget *button_suspend;
+GtkWidget *button_eliminate;
 
-static void seleccionar_estado(GtkButton *button, gpointer user_data) {
-    gint index = GPOINTER_TO_INT(user_data);
-    estado_seleccionado = index;
-    for (int i = 0; i < 5; ++i) {
-        gtk_widget_set_sensitive(estado_buttons[i], TRUE);
-    }
-    gtk_widget_set_sensitive(GTK_WIDGET(button), FALSE);
-}
 
 static void aplicar_filtros(GtkButton *button, gpointer user_data) {
-    GtkTextBuffer *buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(textview));
-    gtk_text_buffer_set_text(buffer, "", -1);
-
    
-    char *pid_text    = g_strdup(gtk_editable_get_text(GTK_EDITABLE(entry_pid)));
-    char *uid_text    = g_strdup(gtk_editable_get_text(GTK_EDITABLE(entry_uid)));
-    char *gid_text    = g_strdup(gtk_editable_get_text(GTK_EDITABLE(entry_gid)));
-    char *nombre_text = g_strdup(gtk_editable_get_text(GTK_EDITABLE(entry_nombre)));
-    char *ppid_text   = g_strdup(gtk_editable_get_text(GTK_EDITABLE(entry_ppid)));
-    char *gpid_text   = g_strdup(gtk_editable_get_text(GTK_EDITABLE(entry_gpid)));
-    char *ram_text    = g_strdup(gtk_editable_get_text(GTK_EDITABLE(entry_ram)));
+    char *pid_text = g_strstrip(g_strdup(gtk_editable_get_text(GTK_EDITABLE(entry_pid))));
+    char *uid_text = g_strstrip(g_strdup(gtk_editable_get_text(GTK_EDITABLE(entry_uid))));
+    char *gid_text = g_strstrip(g_strdup(gtk_editable_get_text(GTK_EDITABLE(entry_gid))));
+    char *estado = g_strstrip(g_strdup(gtk_editable_get_text(GTK_EDITABLE(entry_estado))));
+    char *nombre = g_strstrip(g_strdup(gtk_editable_get_text(GTK_EDITABLE(entry_nombre))));
+    char *ppid = g_strstrip(g_strdup(gtk_editable_get_text(GTK_EDITABLE(entry_ppid))));
+    char *gpid = g_strstrip(g_strdup(gtk_editable_get_text(GTK_EDITABLE(entry_gpid))));
 
-    char *pid    = g_strstrip(pid_text);
-    char *uid    = g_strstrip(uid_text);
-    char *gid    = g_strstrip(gid_text);
-    char *nombre = g_strstrip(nombre_text);
-    char *ppid   = g_strstrip(ppid_text);
-    char *gpid   = g_strstrip(gpid_text);
-    char *ram    = g_strstrip(ram_text);
+    int pid = atoi(pid_text);
+    int uid = atoi(uid_text);
+    int gid = atoi(gid_text);
+    int ppid_num = atoi(ppid);
+    int gpid_num = atoi(gpid);
 
-    GListModel *estado_model = gtk_drop_down_get_model(GTK_DROP_DOWN(dropdown_estado));
-    guint index = gtk_drop_down_get_selected(GTK_DROP_DOWN(dropdown_estado));
-    GtkStringObject *item = GTK_STRING_OBJECT(g_list_model_get_item(estado_model, index));
-    const char *estado = gtk_string_object_get_string(item);
+
+
 }
+
+
+static void activar_modo(GtkButton *button, gpointer user_data) {
+    const gchar *modo = (const gchar *)user_data;
+    
+
+    gtk_widget_set_visible(entry_ram_limit, g_strcmp0(modo, "RAM") == 0);
+    gtk_widget_set_visible(entry_cpu_limit, g_strcmp0(modo, "CPU") == 0);
+
+    gtk_widget_set_visible(button_suspend, TRUE);
+    gtk_widget_set_visible(button_eliminate, TRUE);
+}
+
+
+static void suspender(GtkButton *button, gpointer user_data) {
+     g_print("Procesos suspendidos");
+}
+
+
+static void eliminar(GtkButton *button, gpointer user_data) {
+     g_print("Procesos eliminados");
+}
+
+
+static void on_reanudar_response(GObject *source_object, GAsyncResult *res, gpointer user_data) {
+    GtkAlertDialog *alert = GTK_ALERT_DIALOG(source_object);
+    int response = gtk_alert_dialog_choose_finish(alert, res, NULL);
+
+
+    if (response == 0) {
+    g_print("Reanudar procesos\n");
+    } else {
+    g_print("Los procesos no se reanudarán\n");
+    }
+}
+static void reanudar(GtkWindow *parent_window) {
+    const char *buttons[] = {"Sí", "No", NULL};
+    GtkAlertDialog *alert = gtk_alert_dialog_new("¿Reanudar procesos?");
+    gtk_alert_dialog_set_buttons(alert, buttons);
+
+    gtk_alert_dialog_choose(alert, parent_window, NULL, on_reanudar_response, NULL);
+}
+
+
+static void on_confirmar_salida_response(GObject *source_object, GAsyncResult *res, gpointer user_data) {
+    GtkAlertDialog *alert = GTK_ALERT_DIALOG(source_object);
+    int response = gtk_alert_dialog_choose_finish(alert, res, NULL);
+
+    if (response == 0) {
+        g_print("Reanudar procesos antes de salir\n");
+        reanudar(GTK_WINDOW(user_data));
+    } else {
+        g_print("Saliendo sin reanudar procesos\n");
+        gtk_window_destroy(GTK_WINDOW(user_data));
+    }
+
+    g_object_unref(alert);
+}
+
+static gboolean confirmar_salida(GtkWindow *window, gpointer user_data) {
+    const char *buttons[] = {"Sí", "No", NULL};
+    GtkAlertDialog *alert = gtk_alert_dialog_new("¿Reanudar procesos?");
+    gtk_alert_dialog_set_buttons(alert, buttons);
+
+    gtk_alert_dialog_choose(alert, window, NULL, on_confirmar_salida_response, NULL);
+
+    return TRUE;
+}
+
 static void activate(GtkApplication *app, gpointer user_data) {
     GtkWidget *window = gtk_application_window_new(app);
     gtk_window_set_title(GTK_WINDOW(window), "Filtro de Procesos");
-    gtk_window_set_default_size(GTK_WINDOW(window), 600, 500);
+    gtk_window_set_default_size(GTK_WINDOW(window), 500, 500);
 
     GtkWidget *grid = gtk_grid_new();
     gtk_grid_set_row_spacing(GTK_GRID(grid), 5);
@@ -64,31 +119,60 @@ static void activate(GtkApplication *app, gpointer user_data) {
     ADD_ENTRY("Nombre:", entry_nombre);
     ADD_ENTRY("PPID:", entry_ppid);
     ADD_ENTRY("GPID:", entry_gpid);
-    ADD_ENTRY("Límite RAM:", entry_ram);
+    gtk_grid_attach(GTK_GRID(grid),
+    gtk_label_new(
+        "Estados posibles:\n"
+        "F (foreground), B (background)\n"
+        "R (Running), S (Sleeping), Z (Zombie)\n"
+        "T (Suspendido), D (Espera ininterrumpida)"
+    ),
+    0, row++, 2, 1
+    );
+    ADD_ENTRY("Estado:", entry_estado);
 
-
-    for (int i = 0; i < 5; ++i) {
-        estado_buttons[i] = gtk_button_new_with_label(estados[i]);
-        g_signal_connect(estado_buttons[i], "clicked", G_CALLBACK(seleccionar_estado), GINT_TO_POINTER(i));
-        gtk_grid_attach(GTK_GRID(grid), estado_buttons[i], i % 2, row + i / 2 + 1, 1, 1);
-    }
-    gtk_widget_set_sensitive(estado_buttons[0], FALSE);
-    row += 4;
-
-
+    
     GtkWidget *boton = gtk_button_new_with_label("Aplicar Filtros");
     gtk_grid_attach(GTK_GRID(grid), boton, 0, row++, 2, 1);
     g_signal_connect(boton, "clicked", G_CALLBACK(aplicar_filtros), NULL);
 
-    textview = gtk_text_view_new();
-    gtk_text_view_set_editable(GTK_TEXT_VIEW(textview), FALSE);
-    gtk_grid_attach(GTK_GRID(grid), textview, 0, row++, 2, 1);
+    GtkWidget *separator = gtk_separator_new(GTK_ORIENTATION_HORIZONTAL);
+    gtk_grid_attach(GTK_GRID(grid), separator, 0, row++, 2, 1);
+
+    GtkWidget *button_modo_ram = gtk_button_new_with_label("Modo RAM");
+    gtk_grid_attach(GTK_GRID(grid), button_modo_ram, 0, row, 1, 1);
+    g_signal_connect(button_modo_ram, "clicked", G_CALLBACK(activar_modo), "RAM");
+
+    GtkWidget *button_modo_cpu = gtk_button_new_with_label("Modo CPU");
+    gtk_grid_attach(GTK_GRID(grid), button_modo_cpu, 1, row++, 1, 1);
+    g_signal_connect(button_modo_cpu, "clicked", G_CALLBACK(activar_modo), "CPU");
+
+    entry_ram_limit = gtk_entry_new();
+    gtk_entry_set_placeholder_text(GTK_ENTRY(entry_ram_limit), "Límite RAM");
+    gtk_grid_attach(GTK_GRID(grid), entry_ram_limit, 0, row++, 2, 1);
+    gtk_widget_set_visible(entry_ram_limit, FALSE);
+
+    entry_cpu_limit = gtk_entry_new();
+    gtk_entry_set_placeholder_text(GTK_ENTRY(entry_cpu_limit), "Límite CPU");
+    gtk_grid_attach(GTK_GRID(grid), entry_cpu_limit, 0, row++, 2, 1);
+    gtk_widget_set_visible(entry_cpu_limit, FALSE);
+
+    button_suspend = gtk_button_new_with_label("Suspender");
+    gtk_grid_attach(GTK_GRID(grid), button_suspend, 0, row, 1, 1);
+    g_signal_connect(button_suspend, "clicked", G_CALLBACK(suspender), NULL);
+    gtk_widget_set_visible(button_suspend, FALSE);
+
+    button_eliminate = gtk_button_new_with_label("Eliminar");
+    gtk_grid_attach(GTK_GRID(grid), button_eliminate, 1, row++, 1, 1);
+    g_signal_connect(button_eliminate, "clicked", G_CALLBACK(eliminar), NULL);
+    gtk_widget_set_visible(button_eliminate, FALSE);
+
+   
 
     gtk_window_present(GTK_WINDOW(window));
 }
 
 int main(int argc, char **argv) {
-    GtkApplication *app = gtk_application_new("org.ejemplo.filtro_moderno", G_APPLICATION_DEFAULT_FLAGS);
+    GtkApplication *app = gtk_application_new("org.ejemplo.filtro_procesos", G_APPLICATION_DEFAULT_FLAGS);
     g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
     int status = g_application_run(G_APPLICATION(app), argc, argv);
     g_object_unref(app);
